7be0d2d5150b1f33e272886b18d0c07c2728df93
Hemrajjj13 <iamhemraj13@gmail.com>
2025-11-03 18:01:34 +0530

    Initial commit

---
+++ Sheryins/script.js
@@ -0,0 +1,28 @@
+// var let const
+// declaration and initialization
+
+let a; // declaration
+let b = 12;//declaration and initialization
+
+// global scope 
+// function scope
+// block scope
+
+
+// reassignment
+var x =10;
+x =  45;
+
+// reassignment and redeclaration
+var y = 45;
+var y = 39;
+
+
+// temporal dead zone
+console.log(p);
+let p = 12;
+
+//hoisting impact per type 
+
+
+//object.freeze
